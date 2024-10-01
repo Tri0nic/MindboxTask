6dfb5408b4aea52c34d1112ec5c8589939704980
Tri0nic <androyd0007@yandex.ru>
2024-10-02 00:30:13 +0300

    feat: добавил проверку на корректность данных и проверку треугольника на прямоугольность

--- MindboxLibrary/Triangle.cs
+++ MindboxLibrary/Triangle.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MindboxLibrary
+﻿namespace MindboxLibrary
 {
     public class Triangle : IShape
     {
@@ -14,6 +8,9 @@ namespace MindboxLibrary
 
         public Triangle(double sideA, double sideB, double sideC)
         {
+            if (!IsValidTriangle(sideA, sideB, sideC))
+                throw new ArgumentException("Стороны не образуют треугольник!");
+
             _sideA = sideA;
             _sideB = sideB;
             _sideC = sideC;
@@ -24,6 +21,19 @@ namespace MindboxLibrary
             double semiPerimeter = (_sideA + _sideB + _sideC) / 2;
 
             return Math.Sqrt(semiPerimeter * (semiPerimeter - _sideA) * (semiPerimeter - _sideB) * (semiPerimeter - _sideC));
+        }
+
+        public bool IsRightAngled()
+        {
+            double[] sides = { _sideA, _sideB, _sideC };
+            Array.Sort(sides);
+
+            return Math.Abs(Math.Pow(sides[2], 2) - Math.Pow(sides[1], 2) - Math.Pow(sides[0], 2)) < 0.0001;
+        }
+
+        private bool IsValidTriangle(double a, double b, double c)
+        {
+            return a + b > c && a + c > b && b + c > a;
         }
     }
 }
